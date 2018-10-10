fcd47cc877efb7779c266114bd04a1defe8e1418
Tobik1610 <39301495+Tobik1610@users.noreply.github.com>
2018-10-10 21:24:50 +0200

    Update login.view.php

--- login.view.php
+++ login.view.php
@@ -4,10 +4,11 @@
 <body>
 
 <label for="login"> Anmeldung: </label>
+<div>
   Email: <input name="email" type="text">
   Password: <input name="password" type="text">
   <input type="submit">
-
+</div>
 </body>
 
 </html>
