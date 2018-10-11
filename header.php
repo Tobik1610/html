ec55b9b26eab8b4b77836b4bbd0f57c6edde1421
Tobik1610 <39301495+Tobik1610@users.noreply.github.com>
2018-10-11 18:32:44 +0200

    Header

--- header.php
+++ header.php
@@ -5,10 +5,31 @@
     <meta name="description" content="Example of description">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
-
+      <link rel="stylesheet" href="/css/header.css">
   </head>
   <body>
 
     <header>
-
+      <nav>
+        <a href="#">
+          <img src="img/logo.png" alt="logo">
+        </a>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">About me</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <div>
+          <form action="includes/login.inc.php" method="post">
+            <input type="text" name="mailuid" placeholder="Username/Email">
+            <input type="password" name="pwd" placeholder="Password">
+            <button type="submit" name="login-submit">Login</button>
+          </form>
+          <a href="signup.php">Signup</a>
+          <form action="includes/logout.inc.php" method="post">
+            <button type="submit" name="logout-submit">Logout</button>
+          </form>
+        </div>
+      </nav>
     </header>
